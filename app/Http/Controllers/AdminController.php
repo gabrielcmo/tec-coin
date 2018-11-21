ce4424f98fce4fed94cc75208c2ca9cfa209265d
feluiz3628 <lflf3628@gmail.com>
2018-11-20 23:48:30 -0200

    Adicionando Controllers e View

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User as UserModel;
 use Illuminate\Foundation\Auth\User;
-
+use App\Admin;
+use App\Balance;
+use App\Buyer;
+use App\Seller;
 class AdminController extends Controller
 {
     public function buyers()
@@ -20,28 +23,48 @@ class AdminController extends Controller
         return redirect()->with("users", $users);
     }
 
-    public function deposit()
+    public function deposit(Request $r)
     {
         // validar dados (?)
-
+        // ??
         // Depositar valor
-
+        $idbuyer = Buyer::where('id',$r['id'])->get();
+        $balance = new Balance;
+        $balance->value = $r['value'];
+        $balance->admin_id = 1;
+        $balance->buyer_id = $idbuyer;
+        $balance->description = $r['description'];
+        $balance->date = getdate();
+        $balance->save();
         // Redirecionar para listagem de compradores
-
+        return view('admin.buyerslist');
         // Mostrar mensagem de sucesso (se der tempo)
     }
 
     public function destroyUser($id)
     {
-        // Remover o usuário da tabela mais específica (comprador, admin ou vendedor) 
-
+        // Remover o usuário da tabela mais específica (comprador, admin ou vendedor)
+        $user_select = DB::table('User')->where('id', $id)->value('user_type_id');
+        switch ($user_select) {
+            case 1:
+            Admin::where('user_id', $id)->delete();
+            case 2:
+            Buyer::where('user_id', $id)->delete();
+            case 3:
+            Seller::where('user_id',$id)->delete();
+            case 4:
+            Seller::where('user_id',$id)->delete();
+            case 5:
+            Seller::where('user_id',$id)->delete();
+            break;
+        }
         // Remover os dados da tabela usuário
-
+            User::where('id',$id)->delete();
         // Redirecionar para listagem de usuários
-
+            return  view('admin.userlist')->with('success','Usuarios deletados com sucesso');
         // Mostrar mensagem de sucesso (se der tempo)
     }
-    
+
     public function massRegister()
     {
         return redirect("admin.massregister")->with("users", $users);
--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -3,56 +3,90 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Order;
+use App\Seller;
+use App\Buyer;
+use App\OrderStatus;
+use App\Product;
 
 class SellerController extends Controller
 {
 
-    public function products()
+    public function products($id)
     {
         // Buscar todos os produtos do tipo de vendedor logado
+        // Comment: Não tem nada ligando os produtos com o vendendo
         // e chamar a view de listagem de produtos (passandos esses produtos)
-    }
-    
-    public function editProduct()
-    {
-        // Chamar a view de edição de produtos
+        return view('listproduct')->with('listproducts');
     }
 
-    public function updateProduct()
+    public function editProduct()
+    {
+        //Chamar a view de produtos
+        return view ('editproduct');
+    }
+
+    public function updateProduct(Request $r , $id)
     {
         // Atualizar dados de um produto
+        Product::where('id', $id)->update(['name' => $r['name']]);
+        Product::where('id', $id)->update(['value' => $r['value']]);
+        Product::where('id', $id)->update(['description' => $r['description']]);
+        Product::where('id', $id)->update(['image' => $r['image']]);
+        //Alterar o tipo de Produto?
+        Product::where('id', $id)->update(['type_id' => $r['type']]);
+        return view('home');
     }
 
     public function destroyProduct($id)
     {
         // Excluir produto
+        Product::where('id', $id)->delete();
+        return view('home');
     }
 
     public function createProduct()
     {
         // Devolver form de cadastro de produtos
+        return view('productregister');
     }
 
-    public function storeProduct()
+    public function storeProduct(Request $r)
     {
         // Receber dados do form de cadastro de produtos
+        $product = new Product;
+        $product->name = $r['name'];
+        $product->value = $r['value'];
+        $product->description = $r=['description'];
+        $product->type_id = $r['type'];
+        //Criar uma classe para validar a extensão da imagem?
+        $product->image = $r=['image'];
+        return view('home');
     }
 
-    public function pendingOrders()
+    public function pendingOrders($id)
     {
         // Buscar todos os pedidos pendentes do tipo do vendedor logado
-
+        $idseller = User::where('id', $id)->value('id')->get();
+        $all_idproduct_in_order = Order::where('idseller', $idseller)->value('product_id')->get();
+        $all_order = Order::where('idseller', $idselle)->get();
+        //Não sei se isso vai funcionar...
+        $all_products = Product::whereIn('id', $all_idproduct_in_order)->get();
         // Exibir a tela de listagem de pedidos pendentes
-
+        return view('listorder')->compact('all_order','all_products');
     }
 
     public function acceptOrder($id)
     {
         // Aprovar um pedido
+        Order::where('seller_id',$id)->update(['status_id' => 2]);
+        return view('home');
     }
 
     public function denyOrder($id)
     {
         // Recusar um pedido
+        Order::where('seller_id',$id)->update(['status_id' => 3]);
     }
 }
