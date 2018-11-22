39fe44d1f387ce91b1d04840b98f31392585c6f2
leonardosibela <leonardo_sibela@hotmail.com>
2018-11-22 12:18:23 +0000

    Finalizando navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,8 +32,8 @@
         <div class="collapse navbar-collapse" id="navbarText">
 
             @guest
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                 </ul>
@@ -56,38 +56,58 @@
                     </li>
                 @elseif (Auth::user()->user_type_id == 2)
 
-
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('buyerproducts') }}">Produtos</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('balance') }}">Saldo</a>
+                    </li>
 
                 @elseif (Auth::user()->user_type_id == 3)
-                    
-                @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('pendingorders') }}">Pedidos Pendentes</a>
+                    </li>
 
-                </ul>
-
-                <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Conta
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Produtos
                         </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                            <span class="dropdown-item-text">
-                                <div class="row">
-                                    <img width="30px" height="30px" src="https://static.thenounproject.com/png/20344-200.png" class="rounded-circle">
-                                    <div class="margin-dp">
-                                        <strong>{{ Auth::user()->name }}</strong><br>
-                                    </div>
-                                </div>
-                            </span>
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
+                        
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('productsregister') }}">Cadastro</a>
+                            <a class="dropdown-item" href="{{ route('sellerproducts') }}">Listagem</a>
                         </div>
                     </li>
-                </ul>
+                @endif
+
+            </ul>
+
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Conta
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                        <span class="dropdown-item-text">
+                            <div class="row">
+                                <img width="30px" height="30px" src="https://static.thenounproject.com/png/20344-200.png" class="rounded-circle">
+                                <div class="margin-dp">
+                                    <strong>{{ Auth::user()->name }}</strong><br>
+                                </div>
+                            </div>
+                        </span>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            </ul>
 
             @endguest
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,19 @@ Route::delete('/user/{id}', 'AdminController@destroyUser');
 // Deletar produtos
 
 // buyer
-Route::get('/buyers/products', 'BuyerController@products');
-Route::get('/buyers/balances', 'BuyerController@balance');
+Route::get('/buyers/products', 'BuyerController@products')->name('buyerproducts');
+Route::get('/buyers/balances', 'BuyerController@balance')->name('balance');
 Route::post('/products/order/{id}', 'BuyerController@orderProduct');
 Route::get('/user/profile', 'BuyerController@profile');
 Route::put('/user/profile/update', 'BuyerController@updateProfile');
 
 // seller
-Route::get('/seller/products', 'SellerController@products');
+Route::get('/seller/products', 'SellerController@products')->name('sellerproducts');
 Route::get('/seller/products/edit', 'SellerController@editProduct');
 Route::put('/seller/products/update', 'SellerController@updateProduct');
 Route::delete('seller/products/{id}', 'SellerController@destroyProduct');
-Route::get('/seller/products/create', 'SellerController@createProduct');
+Route::get('/seller/products/create', 'SellerController@createProduct')->name('productsregister');
 Route::post('/seller/products', 'SellerController@storeProduct');
-Route::get('/seller/orders', 'SellerController@pendingOrders');
+Route::get('/seller/orders', 'SellerController@pendingOrders')->name('pendingorders');
 Route::post('/seller/orders/{id}/accept', 'SellerController@acceptOrder');
 Route::post('/seller/orders/{id}/deny', 'SellerController@denyOrder');
