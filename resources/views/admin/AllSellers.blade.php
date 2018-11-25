fe1a39eb15c7117ade9ba66cf4f9b0d7551195cf
bellps <isabella020240@hotmail.com>
2018-11-25 17:46:59 -0200

    Adicionando tela de vendedores e compradores

--- resources/views/admin/AllSellers.blade.php
+++ resources/views/admin/AllSellers.blade.php
@@ -1,13 +1,37 @@
 @extends('layouts.app')
-
 @section('content')
-    Usuário Vendedores <br/>
-    @foreach($sellers as $seller)
-
-        ID -> {{$seller->id}} <br/>
-        Nome -> {{$seller->name}} <br/>
-        Email -> {{$seller->email}} <br/>
-
-    @endforeach
-
+<div class="container">
+        <br>
+        <div class="col-md-12">
+            <div>
+                <h1 class="display-4"><strong>Vendedores cadastrados</strong></h1>
+                <hr>
+            </div>
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Produto ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Senha</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($sellers as $seller)
+                    <tr>
+                        <th scope="row">{{$seller->id}}</th>
+                        <td>?</td>
+                        <td>{{$seller->name}}</td>
+                        <td>{{$seller->email}}</td>
+                        <td>?</td>
+                        <td><a href="#" class="btn btn-warning">Editar</a>  | <a href="#" class="btn btn-danger">Excluir</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/allUser.blade.php
+++ resources/views/admin/allUser.blade.php
@@ -1,14 +1,37 @@
 @extends('layouts.app')
-
 @section('content')
-
-@foreach($AllUsers as $user)
-
-    ID -> {{$user->id}} <br/>
-    Nome -> {{$user->name}} <br/>
-    Email -> {{$user->email}} <br/>
-    User_Type_ID -> {{$user->user_type_id}} <br/>
-
-@endforeach
-
+<div class="container">
+        <br>
+        <div class="col-md-12">
+            <div>
+                <h1 class="display-4"><strong>Usuários cadastrados</strong></h1>
+                <hr>
+            </div>
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Senha</th>
+                        <th scope="col">Saldo</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($AllUsers as $user)
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>?</td>
+                        <td>?</td>
+                        <td><a href="form-edit_user.html" class="btn btn-warning">Editar</a>  | <a href="#" class="btn btn-danger">Excluir</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
