13e271c86870539c12bb0e613aa16ac312befc08
leonardosibela <leonardo_sibela@hotmail.com>
2018-11-26 23:24:00 +0000

    Adicionando funcionalidades de aceitar e recusar pedido

--- app/OrderStatus.php
+++ app/OrderStatus.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderStatus extends Model
 {
+    public static $ORDERED = "1";
+    public static $ACCEPTED = "2";
+    public static $CANCELED = "3";
+    
     protected $fillable = ['description'];
 }
