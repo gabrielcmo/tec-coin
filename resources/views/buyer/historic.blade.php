2d627bd958d81e5c99c2d62dbe93b24826bc91aa
gabriel <gabrieloliveira9669@gmail.com>
2018-11-27 14:20:40 -0200

    historico ok

--- resources/views/buyer/historic.blade.php
+++ resources/views/buyer/historic.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    @if (empty($historic) || !is_array($historic))
+    @if (empty($historic))
         <h1>Como assim você não comprou nada ?! Vá ver alguns produtos!!</h1>
     @else
         @foreach($historic as $historic)
@@ -11,7 +11,6 @@
             ID Comprador -> {{$historic->buyer_id}} <br/>
             ID Vendedor -> {{$historic->seller_id}} <br/>
             Status do Produto -> {{$historic->status_id}} <br/>
-            OrderStatus_ID -> {{$historic->id_order_status}} <br/>
             Valor da Compra -> {{$historic->value}} <br/><br>
 
         @endforeach
